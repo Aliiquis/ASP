b773cad226b361ec17f8f381791fe15bab7258bb
Aliiquis <fran.blazevic0@gmail.com>
2023-10-09 23:06:34 +0200

    Templates test

--- Tests/generics.cpp
+++ Tests/generics.cpp
@@ -3,12 +3,26 @@
 using namespace std;
 
 template<class T>
-T compare(T &first, T &second) {
+bool compare(T &first, T &second) {
     return first < second;
 }
 
+class MyClass {
+public:
+    int id;
+
+    MyClass(int id) : id(id) {};
+
+    friend bool operator<(MyClass &first, MyClass &second);
+};
+
+bool operator<(MyClass &first, MyClass &second) {
+    return first.id < second.id;
+}
+
 int main() {
-    int i = 5, j = 10;
-    cout << compare(i, j) << endl;
+    MyClass myClass0(1);
+    MyClass myClass1(1);
+    cout << compare(myClass0, myClass1) << endl;
     return 0;
 }
